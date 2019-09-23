a20219f715228e63ed88552da9e38401435fde15
Ali <aliseylaneh@live.com>
2019-09-23 15:11:57 +0330

    first things first up to get ready

---
+++ src/main/java/com/aliseylaneh/springmvcspringboot/SpringbootSpringmvcAppApplication.java
@@ -0,0 +1,13 @@
+package com.aliseylaneh.springmvcspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringmvcAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpringmvcAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aliseylaneh/springmvcspringboot/SpringbootSpringmvcAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aliseylaneh.springmvcspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSpringmvcAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
