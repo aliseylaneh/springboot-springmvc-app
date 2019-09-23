8320ac1934831ca5459b8664fdffdec4ee70393a
Ali <aliseylaneh@live.com>
2019-09-24 00:44:46 +0330

    some changes with customerconstoller for resolving the customer page

---
+++ src/main/java/com/aliseylaneh/springmvcspringboot/controllers/CustomerController.java
@@ -0,0 +1,12 @@
+package com.aliseylaneh.springmvcspringboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomerController {
+    @RequestMapping("/customers")
+    public String customers() {
+        return "customers";
+    }
+}
